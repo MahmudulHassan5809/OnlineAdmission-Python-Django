a19a38e0a90651e4a5949eaa9e35fdb5ff704a7f
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2020-06-29 01:56:13 +0600

    Template Setup Done

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.generic.base import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name="home.html")),
 ]
 
 
