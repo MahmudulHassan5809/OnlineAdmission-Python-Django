2c3b469620676dd0e0da1fe0a8e78085877030f9
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2020-06-28 22:05:40 +0600

    Project Start And Setup Django Project

---
+++ myproject/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.FORCE_STATIC_FILE_SERVING and not settings.DEBUG:
+    settings.DEBUG = True
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    settings.DEBUG = False
+
+
+admin.site.site_header = "VortiBD Admin"
+admin.site.site_title = "VortiBD Admin Portal"
+admin.site.index_title = "VortiBD to Finder Researcher Portal"
