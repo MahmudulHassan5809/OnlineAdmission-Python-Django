3b9e3c467c8b9d5e28f96a31169a5e3ec731567d
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2020-07-02 22:21:57 +0600

    Applications Model Created

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from applicant.models import ApplicantProfile
+from institution.models import InstitutionProfile, AdmissionSession
 # Create your models here.
+
+
+class Applications(models.Model):
+    owner = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='owner_applications')
+    applicant = models.ForeignKey(
+        ApplicantProfile, on_delete=models.CASCADE, related_name='applicant_applications')
+    institute = models.ForeignKey(
+        InstitutionProfile, on_delete=models.CASCADE, related_name='institute_applications')
+
+    def __str__(self):
+        return f"{self.applicant.student_name} apply to {self.institute.institute_name}"
