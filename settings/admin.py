b70a3f24e106823f5f8683b7b154fc0a8d5dc3a4
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2020-07-06 17:00:17 +0600

    SiteInfo & SiteFaq models created and register to admin.py

--- settings/admin.py
+++ settings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ApplicationInstruction, ApplicationInstructionList
+from .models import ApplicationInstruction, ApplicationInstructionList, SiteInfo, SiteFaq
 # Register your models here.
 
 
@@ -18,3 +18,25 @@ class ApplicationInstructionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(ApplicationInstruction, ApplicationInstructionAdmin)
+
+
+class SiteInfoAdmin(admin.ModelAdmin):
+    list_display = ['site_name', 'site_phone', 'site_email']
+
+    def has_add_permission(self, request):
+        return False if self.model.objects.count() > 0 else True
+
+
+admin.site.register(SiteInfo, SiteInfoAdmin)
+
+
+class SiteFaqAdmin(admin.ModelAdmin):
+    list_display = ['question', 'answer']
+    search_fields = ['question', 'answer']
+    list_per_page = 20
+
+    def has_add_permission(self, request):
+        return False if self.model.objects.count() > 0 else True
+
+
+admin.site.register(SiteFaq, SiteFaqAdmin)
--- settings/models.py
+++ settings/models.py
@@ -3,12 +3,25 @@ from django.db import models
 # Create your models here.
 
 
+class SiteInfo(models.Model):
+    site_name = models.CharField(max_length=255)
+    site_phone = models.CharField(max_length=20)
+    site_email = models.EmailField()
+
+    class Meta:
+        verbose_name = 'SiteInfo'
+        verbose_name_plural = '1.SiteInfo'
+
+    def __str__(self):
+        return site_name
+
+
 class ApplicationInstruction(models.Model):
     title = models.CharField(max_length=255)
 
     class Meta:
         verbose_name = 'Application Instruction'
-        verbose_name_plural = '1.Application Instruction'
+        verbose_name_plural = '2.Application Instruction'
 
     def __str__(self):
         return self.title
@@ -21,7 +34,19 @@ class ApplicationInstructionList(models.Model):
 
     class Meta:
         verbose_name = 'Instruction'
-        verbose_name_plural = '2. Instruction'
+        verbose_name_plural = '3. Instruction'
 
     def __str__(self):
         return self.name
+
+
+class SiteFaq(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'FAQ'
+        verbose_name_plural = '4.FAQ'
+
+    def __str__(self):
+        return self.question
