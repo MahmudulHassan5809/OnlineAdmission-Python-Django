348f6a086ae6429ad867ecb66c942938f45178e4
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2020-07-13 23:17:36 +0600

    Fix Bugs

--- institution/context_processors.py
+++ institution/context_processors.py
@@ -3,10 +3,11 @@ from institution.models import InstitutionProfile, AdmissionSession
 
 def on_going_admission(request):
     on_going_admission = AdmissionSession.objects.select_related(
-        'institute').filter(status=True)
+        'institute').filter(status=True).exclude(institute__institute_pic='')
     return {'on_going_admission': on_going_admission}
 
 
 def all_institute_profile(request):
-    all_institute_profile = InstitutionProfile.objects.filter(active=True)
+    all_institute_profile = InstitutionProfile.objects.filter(
+        active=True).exclude(institute_pic='')
     return {'all_institute_profile': all_institute_profile}
